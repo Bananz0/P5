e47489a1f5a8355b5ad0cfe84d780d6ca4518c94
glenm@XPS <glenmuthokaq@gmail.com>
2024-03-01 00:33:28 +0000

    Added Gate class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+class Gate {
+public:
+    bool A, B,C;
+    Gate(bool aIn, bool bIn) : A(aIn), B(bIn){}
+    virtual bool getOutput(){
+        return C;
+    }
+};
+
+class XOR : public Gate {
+    bool getOutput() override{
+      C = A ^ B;
+      return C;
+    }
+};
+
+class AND : public Gate {
+    bool getOutput() override{
+        C = A & B;
+        return C;
+    }
+};
+
+class OR : public Gate {
+    bool getOutput() override{
+        C = A | B;
+        return C;
+    }
+};
+class NOT : public Gate {
+    bool getOutput() override{
+        //Assume B is inexistent
+        C = !A;
+        return C;
+    }
+};
+
+
+
+int main() {}
